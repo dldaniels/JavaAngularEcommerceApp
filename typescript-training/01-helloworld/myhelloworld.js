e80962b85ba5521a0a87ad5816fd1b5b428932bc
David Daniels <dldaniels07@yahoo.com>
2023-07-30 18:03:15 -0600

    Added relevant training folder

---
+++ typescript-training/01-helloworld/myhelloworld.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log("I like " + "Pizza" + "!");
+var firstName = "David";
+var lastName = "Daniels";
+var jerseyNumber = 13;
+var playsSport = true;
+console.log("Hi " + firstName + " " + lastName + "!");
+console.log("Hi ".concat(firstName, " ").concat(lastName, "!"));
+var reviews = [5, 5, 4.5, 1, 3];
+for (var i = 0; i < reviews.length; i++) {
+    console.log(reviews[i]);
+}
