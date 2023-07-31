4a3eb7fc7361a438b55b00a39c1629c40672dce7
David Daniels <dldaniels07@yahoo.com>
2023-07-30 18:11:17 -0600

    helloworld

--- typescript-training/01-helloworld/myhelloworld.ts
+++ typescript-training/01-helloworld/myhelloworld.ts
@@ -1,16 +1,5 @@
 console.log("Hello World");
 console.log("I like " + "Pizza" + "!");
 
-let firstName: string = "David";
-let lastName: string = "Daniels";
-let jerseyNumber: number = 13;
-let playsSport: boolean = true;
 
-console.log("Hi " + firstName + " " + lastName + "!");
-console.log(`Hi ${firstName} ${lastName}!`);
 
-let reviews: number[] = [5, 5, 4.5, 1, 3];
-
-for (let i=0; i < reviews.length; i++) {
-    console.log(reviews[i]);
-}
